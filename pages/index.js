fa5c74d3147a204db859a54e2dc609af6bcf2996
santicano <canosantiago404@gmail.com>
2024-01-26 15:29:30 -0300

    añadida foto de perfil

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue
+} from '@chakra-ui/react'
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} textAlign="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        textAlign="center"
+      >
         Hola, soy un desarrollador web de Argentina!
       </Box>
 
@@ -13,6 +25,30 @@ const Page = () => {
             Santiago Cano
           </Heading>
           <p>FullStack developer</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Box
+            borderColor="whiteAplha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <Image
+              src="/images/santi.jpeg"
+              alt="Foto de perfil"
+              width="100"
+              height="100"
+            />
+          </Box>
         </Box>
       </Box>
     </Container>
