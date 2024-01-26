d831e21bfbb62d7c10a073e11e81da2602ad7ed3
santicano <canosantiago404@gmail.com>
2024-01-26 00:13:44 -0300

    added main layout

---
+++ components/layouts/main.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import Head from 'next/head'
+import { Box, Container } from '@chakra-ui/react'
+
+const Main = ({ children, router }) => {
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Santiago Cano - Homepage</title>
+      </Head>
+
+      <Container maxW="container.md" pt={14}>
+        {children}
+      </Container>
+    </Box>
+  )
+}
+
+export default Main
