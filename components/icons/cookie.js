c1c183f13856e4a5fb65386262fe61022008e6ee
santicano <canosantiago404@gmail.com>
2024-01-26 14:59:26 -0300

    cambiada fuente global, arreglado clases de cookie icon

--- components/icons/cookie.js
+++ components/icons/cookie.js
@@ -2,16 +2,15 @@ const CookieIcon = props => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      class="icon icon-tabler icon-tabler-cookie"
       className="w-5 inline-block transition-transform group-hover:rotate-[20deg]"
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      stroke-width="2"
+      strokeWidth="2"
       stroke="currentColor"
       fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
