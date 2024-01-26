57d0108b6b19416a9f3f4353db1b84dcf7786471
santicano <canosantiago404@gmail.com>
2024-01-26 10:57:03 -0300

    añadido esqueleto de la pagina

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
 const Page = () => {
-  return <div>Hola</div>
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hola, soy un desarrollador web de Argentina!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Santiago Cano
+          </Heading>
+          <p>Front end developer con conocimientos de back end</p>
+        </Box>
+      </Box>
+      <div>Hola</div>
+    </Container>
+  )
 }
 
 export default Page
