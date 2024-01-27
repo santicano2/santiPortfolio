0eca14f67bd9647f10323189a6b9d56e6e619cb8
santicano <canosantiago404@gmail.com>
2024-01-27 18:11:14 -0300

    agregados mas proyectos

--- pages/projects.js
+++ pages/projects.js
@@ -4,11 +4,13 @@ import { WorkGridItem } from '../components/grid-item'
 
 import thumbWilly from '../public/images/works/thumbnail_willy.png'
 import thumbWeText from '../public/images/works/thumbnail_wetext.png'
+import thumbOdinBook from '../public/images/works/thumbnail_odinbook.png'
+import thumbsShopCart from '../public/images/works/thumbnail_shopcart.png'
 
 const Works = () => {
   return (
     <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
+      <Heading as="h3" fontSize={20} my={4}>
         Proyectos
       </Heading>
 
@@ -23,8 +25,27 @@ const Works = () => {
           </WorkGridItem>
         </Section>
         <Section>
-          <WorkGridItem id="WeText" title="WeText" thumbnail={thumbWeText}>
+          <WorkGridItem id="wetext" title="WeText" thumbnail={thumbWeText}>
             Una app web de mensaje de texto
+          </WorkGridItem>
+        </Section>
+
+        <Section delay={0.1}>
+          <WorkGridItem
+            id="odinbook"
+            title="OdinBook"
+            thumbnail={thumbOdinBook}
+          >
+            Red social imitación de Facebook
+          </WorkGridItem>
+        </Section>
+        <Section delay={0.1}>
+          <WorkGridItem
+            id="shopcart"
+            title="Shop Cart"
+            thumbnail={thumbsShopCart}
+          >
+            Un sitio web de compra de ropa falsa
           </WorkGridItem>
         </Section>
       </SimpleGrid>
