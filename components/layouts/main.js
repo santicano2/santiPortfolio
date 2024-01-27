abc923e2f54b32d97226a1de2b32f09584b38a09
santicano <canosantiago404@gmail.com>
2024-01-27 14:02:46 -0300

    añadido pagina projects, por completar

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -7,7 +7,7 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Santiago Cano - Homepage</title>
+        <title>Home - Santiago Cano</title>
       </Head>
 
       <Navbar path={router.asPath} />
