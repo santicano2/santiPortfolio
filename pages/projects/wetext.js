5abc1b9e9e2530c1ebc5b82567c13c0a9946c50f
santicano <canosantiago404@gmail.com>
2024-01-28 14:31:41 -0300

    añadida pagina de weText

---
+++ pages/projects/wetext.js
@@ -0,0 +1,45 @@
+import {
+  Container,
+  Badge,
+  Link,
+  List,
+  ListItem,
+  AspectRatio
+} from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Title, WorkImage, Meta } from '../../components/project'
+import Paragraph from '../../components/paragraph'
+import Layout from '../../components/layouts/article'
+
+const Project = () => (
+  <Layout title="Where's Wally">
+    <Container my={6}>
+      <Title>
+        WeText <Badge>2024</Badge>
+      </Title>
+      <Paragraph>
+        App para mandar mensajes entre usuarios, con autentificación y
+        almacenamiento de imagenes
+      </Paragraph>
+      <List ml={4} my={4}>
+        <ListItem>
+          <Meta>Sitio Web</Meta>
+          <Link href="https://top-we-text.vercel.app/">
+            https://top-we-text.vercel.app/
+            <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Meta>Stack</Meta>
+          <span>React, NodeJS, Socket.io, Cloudinary, MongoDB</span>
+        </ListItem>
+      </List>
+
+      <WorkImage src="/images/projects/thumbnail_wetext.png" alt="WeText" />
+      <WorkImage src="/images/projects/wetext.png" alt="WeText" />
+      <WorkImage src="/images/projects/wetext2.png" alt="WeText" />
+    </Container>
+  </Layout>
+)
+
+export default Project
