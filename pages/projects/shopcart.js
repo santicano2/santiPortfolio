9de4870b0fbdc74cc8c4f322d20601df84ba9fb8
santicano <canosantiago404@gmail.com>
2024-01-29 19:26:54 -0300

    quitados imports innecesarios

--- pages/projects/shopcart.js
+++ pages/projects/shopcart.js
@@ -1,11 +1,4 @@
-import {
-  Container,
-  Badge,
-  Link,
-  List,
-  ListItem,
-  AspectRatio
-} from '@chakra-ui/react'
+import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, WorkImage, Meta } from '../../components/project'
 import Paragraph from '../../components/paragraph'
